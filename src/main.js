fc04b1df9919ffca619a3d162b57dba4cf1dd145
xx <578742871@qq.com>
2023-11-11 14:59:01 +0800

    提交代码

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/tf1Ra65n92WFeAjV/scene.splinecode');

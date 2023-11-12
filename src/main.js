78ff6b4112cd813717696d089ca540a5494dead7
xx <578742871@qq.com>
2023-11-12 15:08:40 +0800

    修改

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/tf1Ra65n92WFeAjV/scene.splinecode');
